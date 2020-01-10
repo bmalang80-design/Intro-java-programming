3398aade18f1e7b8968c01591a1446ae04106e45
bmalang80-design <58871504+bmalang80-design@users.noreply.github.com>
2020-01-10 00:55:38 -0500

    initial

--- src/chapter33/Exercise33_01Server.java
+++ src/chapter33/Exercise33_01Server.java
@@ -3,6 +3,7 @@ package chapter33;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 public class Exercise33_01Server {
 
@@ -13,10 +14,12 @@ public class Exercise33_01Server {
     public Exercise33_01Server(){
         new Thread(()->{
             try {
+
                 ServerSocket serverSocket = new ServerSocket(9000);
-                System.out.println("Server has started");
+                System.out.println("Exercise33_01Server started at at "+ new Date());
                 while (true){
                     Socket socket = serverSocket.accept();
+                    System.out.println("Connected to client at "+ new Date());
                     HandleClientSocket socket1 = new HandleClientSocket(socket);
                     new Thread(socket1).start();
                 }
